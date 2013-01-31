f531d5b8a7a41a7e4abaa952cc4da68c2a1daf6c
Manuel Schultheiß <manukru@gmail.com>
2013-01-31 23:00:55 +0100

    receiver

--- web/receiver/index.php
+++ web/receiver/index.php
@@ -6,11 +6,21 @@ $action  =($_POST["action"]);
 $username = ($_POST["receiver"]);
 $data = json_decode(($_POST["json"]), true); //Second parameter ensures return value is array
 
-
-
-
-
 include("functions.php");
+
+if (isset($_POST["multi"]) && $_POST["multi"] == true) 
+{
+	$bigArray = array();
+
+	foreach ($data as $item)
+	{
+		$bigArray[] = parseRequest($item["rqid"], $username, $item["data"], $_POST["sender"]);
+
+	}
+
+	echo json_encode($bigArray);
+}
+else
 echo json_encode(parseRequest($action, $username, $data, $_POST["sender"]));
 
 ?>
