0a0742a28aa0241d018b236760568f7a44d25524
Manuel Schultheiß <manukru@gmail.com>
2014-10-23 23:28:08 +0200

    bugfix

--- server/charme/fs.php
+++ server/charme/fs.php
@@ -66,7 +66,6 @@ else
 header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 
 
-header('Access-Control-Allow-Origin: http://client.local');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); 
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true'); // Needed for CORS Cookie sending
